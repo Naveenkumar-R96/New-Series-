1ade365fd4e7705383c2bca4487eaff840952d6d
NAVEEN KUMAR <nk946432@gmail.com>
2025-02-15 20:48:08 +0530

    today done

---
+++ src/First_phase/Basics/TypeCast.java
@@ -0,0 +1,23 @@
+package First_phase.Basics;
+
+public class TypeCast {
+    public static void main(String[] args) {
+       /* int a=4;
+        if (a>5){
+            System.out.println("hellow world");
+        }
+        else{
+            System.out.println("ok");
+        }*/
+
+   /*     int count =1;
+        while(count !=5){
+            System.out.println(count);
+            count++;
+        }*/
+
+        for(int count=1; count !=5; count++){
+            System.out.println(count);
+        }
+    }
+}
