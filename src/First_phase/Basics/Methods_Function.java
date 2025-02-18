0726e9bedea1a967cdc6f7b7b2b8181f76abc1bf
NAVEEN KUMAR <nk946432@gmail.com>
2025-02-18 21:14:22 +0530

    some more buddy

--- src/First_phase/Basics/Methods_Function.java
+++ src/First_phase/Basics/Methods_Function.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Methods_Function {
     public static void main(String[] args) {
-        int message=sum3(11,23);
+      /*  int message=sum3(11,23);
         System.out.println(message);
 
 
@@ -12,9 +12,14 @@ public class Methods_Function {
         Scanner in =new Scanner(System.in);
         String namee=in.next();
         String name=myGreet(namee);
-        System.out.println(name);
+        System.out.println(name);*/
+
+       int cum= naveeen(11);
+        System.out.println(cum);
 
     }
+
+
 
 
     /*static String sum(){
@@ -22,7 +27,7 @@ public class Methods_Function {
         return greeting;
     }*/
 
-    static int sum3(int a ,int b){
+  /*  static int sum3(int a ,int b){
         int sum=a+b;
         return sum;
     }
@@ -30,5 +35,13 @@ public class Methods_Function {
     static String myGreet(String in){
         String message="hello "+ in;
         return message;
+    }*/
+
+    static int naveeen(int num){
+        int num1=10;
+        int sum=num+num1;
+        System.out.println("haaai");
+        return sum;
+
     }
 }
---
+++ src/First_phase/Basics/scope.java
@@ -0,0 +1,12 @@
+package First_phase.Basics;
+
+public class scope {
+    public static void main(String[] args) {
+        //already know about some concept
+/*            int a=43;
+        {
+             a=78;
+             int c=98;
+        }*/
+    }
+}
