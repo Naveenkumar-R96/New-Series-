57562247d60beaa5444b8a3db182a76f4d51de5b
NAVEEN KUMAR <nk946432@gmail.com>
2025-03-27 21:30:16 +0530

    leetcode prob

---
+++ src/practicing/one/lengthstring.java
@@ -0,0 +1,18 @@
+package practicing.one;
+
+import java.util.Arrays;
+
+public class lengthstring {
+    public static void main(String[] args) {
+        String [] arr = {"h","e","l","l","o"};
+        String str = Arrays.toString(arr); // Output: [Hello, World, Java]
+        int right=arr.length-1;
+        System.out.println(str);
+         System.out.println(func(arr));
+    }
+    static String[] func(String[] value){
+        int left=0;
+        int right=value.length-1;
+        return new String[]
+    }
+}
