a6235fbcd6787daeb71f825ae609ae00420032ca
NAVEEN KUMAR <nk946432@gmail.com>
2025-06-18 13:59:43 +0530

    solving leetcode

---
+++ src/practicing/one/leet88.java
@@ -0,0 +1,51 @@
+package practicing.one;
+
+import java.util.Arrays;
+
+public class leet88 {
+    public static void main(String[] args) {
+       /* int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int[] nums1 = nums;
+        int k = 5;
+
+        int dig = nums.length - k;
+        System.out.println(dig);
+
+        int index = 0;
+        int n=k;
+
+        for (int i = nums.length - k; i < nums.length; i++) {
+            int current=nums[index];
+            nums[index] = nums[i];
+            nums[n]=current;
+            n++;
+            index++;
+        }
+        *//*int index2=0;
+        for (int i=k;i<nums.length;i++){
+            nums[i]=nums1[index2];
+            index2++;
+        }*//*
+        System.out.println(Arrays.toString(nums));*/
+
+        String word="abcdefd";
+        char ch='d';
+        int ind=-1;
+        for (int i=0;i<word.length();i++){
+            if(word.charAt(i)==ch){
+                ind=i;
+                break;
+            }
+        }
+        int left=0;
+        int right=ind;
+
+        while(left<right){
+            char temp=word.charAt(left);
+            word.charAt(left)=word.charAt(right);
+            word.charAt(right)=temp;
+            left++;
+            right++;
+        }
+    }
+}
